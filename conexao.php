aa60a9481e8147fed37b19f5c9490a618a3bd464
Renan <137122105+Rodrigues-Renan@users.noreply.github.com>
2024-06-17 20:36:10 -0300

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,10 +1,10 @@
 <?php
     session_start();
 
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $dbname = 'contato';
+    $host = 'db4free.net';
+    $user = 'brunotznr_0369';
+    $pass = 'Mysql@0369';
+    $dbname = 'blocozin_0369';
 
     try {
         $conn = new PDO("mysql:host=$host;dbname=$dbname;", $user, $pass);
